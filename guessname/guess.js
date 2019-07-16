15419d65107bcae0b134a7a145b0e7d89bcda066
JC Tovar <jctovar.create@gmail.com>
2019-07-16 14:32:52 -0500

    simple DOM Manipulation - changing display
    Do you think you can guess my Tortoise's name?

---
+++ guessname/guess.js
@@ -0,0 +1,6 @@
+
+document.getElementById('answer').style.display = "none";
+
+document.querySelector('#stylebtn').addEventListener('click', function(){
+    document.getElementById('answer').style.display = "block";
+});
