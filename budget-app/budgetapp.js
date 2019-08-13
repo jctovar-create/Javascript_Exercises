6105bd909fd92c6951a16d7f94635ce2710b63fe
JC Tovar <jctovar.create@gmail.com>
2019-08-13 00:25:39 -0500

    JS Update - totals in data structure and UI

--- budget-app/budgetapp.js
+++ budget-app/budgetapp.js
@@ -57,6 +57,20 @@ var budgetController = (function(){
 
         },
 
+        deleteItem: function(type, id){
+            var ids, index;
+            
+            ids = data.allItems[type].map(function(current){
+                return current.id;
+            });
+
+            index = ids.indexOf(id);
+
+            if(index !== -1){
+                data.allItems[type].splice(index, 1);
+            }
+        },
+
         calculateBudget:function(){
 
             //calculate total income and expenses
@@ -135,6 +149,11 @@ var UIController = (function(){
             //Insert HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHTML);
 
+        },
+
+        deleteListItem: function(selectorID){
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
         },
 
         clearFields: function(){
@@ -232,7 +251,14 @@ var controller = (function(budgetCtrl, UICtrl){
         if (itemID){
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = splitID[1];
+            ID = parseInt(splitID[1]);
+
+            //delete item from data structure
+            budgetCtrl.deleteItem(type, ID);
+            //delete item from the UI 
+            UICtrl.deleteListItem(itemID);
+            //update and show new budget and totals
+            updateBudget();
         };
 
     };
