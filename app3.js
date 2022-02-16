58613c2860c97e561a4d18beef1d2f23a338c31b
JC Tovar <jctovar.create@gmail.com>
2022-02-15 23:24:42 -0600

    Add files via upload

---
+++ app3.js
@@ -0,0 +1,18 @@
+console.log('Its working');
+
+const h1 = document.querySelector('h1');
+
+h1.innerText = 'this is not HEB';
+
+const ul = document.querySelector('ul');
+
+const paragraph = document.querySelector('p');
+
+const form = document.querySelector('form');
+
+const inputs = document.querySelectorAll('input');
+
+const userName = document.querySelector('#username');
+const userPassword = document.querySelector('#password');
+const userCheck = document.querySelector('#checkbox');
+const userSlider = document.querySelector('#slider');
